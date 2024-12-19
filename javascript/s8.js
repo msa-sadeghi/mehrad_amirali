bf4cbd9ca603fa1fbb755aa28b949a925bcd78ce
msa-sadeghi <sadeghi.msai@gmail.com>
2024-12-19 16:54:35 +0330

    calculator html and css

---
+++ javascript/s8.js
@@ -0,0 +1,26 @@
+// "use strict";
+// x = 0;
+// console.log(x);
+// var i = 0;
+// function myFunction() {
+//   console.log(`hi ${i++}`);
+// }
+
+// setInterval(myFunction, 1000);
+// let timeElement = document.getElementById("time");
+// timeElement.style.fontSize = "32px";
+// function showTime() {
+//   let d = new Date();
+//   timeElement.innerHTML = d.toLocaleTimeString();
+// }
+
+// setInterval(showTime, 1000);
+
+// x = 0;
+
+// var i = 0;
+// function myFunction() {
+//   console.log(`hi ${i++}`);
+//   setTimeout(myFunction, 1000);
+// }
+// myFunction();
