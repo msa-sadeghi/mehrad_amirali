a863f25fe10ee73eb9b823bc96f3b0c3ce6a1d55
msa-sadeghi <sadeghi.msai@gmail.com>
2025-01-30 16:57:48 +0330

    a

---
+++ javascript/section2/script.js
@@ -0,0 +1,70 @@
+/*var userCount = 2345;
+var x = 12;
+var minutes = 234;
+var myName = "ali";
+
+var isOnline = true;
+var isMale = false;
+*/
+//alert(typeof isMale);
+
+// var userInfo = prompt("enter something");
+// alert(userInfo)
+
+// var userAge = 19;
+// var userStringAge = String(userAge);
+
+// alert(typeof userStringAge);
+
+// var userNumberAge = Number(userStringAge);
+// alert(typeof userNumberAge);
+// var number1 = prompt("enter number 1:")
+// var number2 = prompt("enter number 2:")
+
+// var number1TypeNumber = +number1
+// var numberT2ypeNumber = +number2
+
+// var plusResult = number1TypeNumber + numberT2ypeNumber
+// var subResult = number1TypeNumber - numberT2ypeNumber
+// var mulResult = number1TypeNumber * numberT2ypeNumber
+// var divResult = number1TypeNumber / numberT2ypeNumber
+
+// alert(mulResult)
+
+// var siteName = "niklearining.ir";
+// var courseName = "javascript";
+
+// alert("Site: " + siteName + "\n" + "Course" + courseName)
+
+// var str1 = "niklearning.ir";
+// var num1 = 12;
+// var num2 = 4;
+
+// alert(str1 / num2);
+
+// alert(typeof NaN);
+
+// alert("1fff2" / 4);
+
+// var str1 = "niklearinign.ir";
+// var x = 12;
+// var y = str1 + x;
+// alert(typeof y);
+// alert(isNaN(str1 + x));
+
+var userNumber = prompt("enter a number:");
+if (userNumber % 2 == 0) {
+  alert(`${userNumber} is even`);
+} else {
+  alert(`${userNumber} is odd`);
+}
+
+
+// سن کاربر را دریافت کنید.
+// اگر سن کاربر کمتر از 18 بود، به او پیغام "شما مجاز به ورود نیستید" را نمایش بدهید
+//  و در غیر این صورت پیغام "ورود به پنل" را نمایش بدهید.
+
+
+// دو عدد را از کاربر دریافت کرده و عدد اول را به توان دومی برسانید و حاصل را نمایش دهید
+
+//یک زمان را بر حسب دقیقه از کاربر دریافت کرده و آن را به ساعت تبدیل کنید و سپس نمایش دهید
