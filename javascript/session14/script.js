1f3402f139ae8977a603907940d4343e003feb46
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-05-22 16:42:16 +0330

    session14.1

--- javascript/session14/script.js
+++ javascript/session14/script.js
@@ -1,5 +1,16 @@
-function scrollHandler(){
-    console.log(document.documentElement.scrollTop)
-}
+const mainElement = document.getElementById('mainNav')
+const logoImg = document.getElementsByTagName('img')[0]
 
-document.addEventListener("scroll", scrollHandler)
+
+document.addEventListener('scroll', function(){
+    if (document.documentElement.scrollTop> 0){
+        logoImg.style.height = '64px'
+        mainElement.classList.add('bg-black')
+        mainElement.classList.add('txt-white')
+    }
+    else{
+        logoImg.style.height = '85px'
+        mainElement.classList.remove('bg-black')
+        mainElement.classList.remove('txt-white')
+    }
+})
