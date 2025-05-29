162585be556a7b052bcfc84663976c92603430f9
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-05-29 16:19:08 +0330

    dark theme

---
+++ javascript/session15/script.js
@@ -0,0 +1,11 @@
+// function insert(){
+//     localStorage.setItem('name', 'sara')
+// }
+// function get(){
+//     let name = localStorage.getItem('name')
+//     console.log(name)
+// }
+
+// function clearData(){
+//     localStorage.clear()
+// }
