244349561100e22e22c6cbbf5dc617f956fc89ba
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-05-29 16:42:01 +0330

    todo first

--- javascript/session15/script.js
+++ javascript/session15/script.js
@@ -9,3 +9,26 @@
 // function clearData(){
 //     localStorage.clear()
 // }
+
+const switchElem = document.querySelector('.switch')
+switchElem.addEventListener('click', function(){
+    document.body.classList.toggle('dark')
+    if (document.body.className.includes('dark')){
+        localStorage.setItem('theme', 'dark')
+    }else{
+        localStorage.setItem('theme', 'light')
+    }
+})
+
+window.onload = function(){
+    let t = localStorage.getItem('theme')
+    if(t === 'dark'){
+        document.body.classList.add('dark')
+    }
+}
+// window.onload = ()=>{
+//     let t = localStorage.getItem('theme')
+//     if(t === 'dark'){
+//         document.body.classList.add('dark')
+//     }
+// }
