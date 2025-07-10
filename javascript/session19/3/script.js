6cd83fa479a5993d6580d5f464f54ec074dc515b
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-07-10 16:29:19 +0330

    section1

--- javascript/session19/3/script.js
+++ javascript/session19/3/script.js
@@ -4,3 +4,36 @@ let allProducts = [
     {id:3, title:'Album 3', price:7, img:'./Images/Album 3.png', count:1},
     {id:4, title:'Album 4', price:9, img:'./Images/Album 4.png', count:1},
 ]
+
+const shopItemsContainer = document.querySelector('.shop-items')
+
+allProducts.forEach(function(product){
+    let productContainer = document.createElement('div')
+    productContainer.classList.add('shop-item')
+    
+    let productTitleSpan = document.createElement('span')
+    productTitleSpan.classList.add('shop-item-title')
+    productTitleSpan.innerHTML = product.title
+    
+    let productImageElem = document.createElement('img')
+    productImageElem.classList.add('shop-item-image')
+    productImageElem.setAttribute('src', product.img)
+    
+    let productDetailsContainer = document.createElement('div')
+    productDetailsContainer.classList.add('shop-item-details')
+    
+    let productPriceSpan = document.createElement('span')
+    productPriceSpan.classList.add('shop-item-price')
+    productPriceSpan.innerHTML = product.price
+
+    let productAddButton = document.createElement('button')
+    productAddButton.innerHTML = "Add To Card"
+    productAddButton.className = 'btn btn-primary shop-item-button'
+
+    productDetailsContainer.append(productPriceSpan, productAddButton)
+    productContainer.append(productTitleSpan, productImageElem, productDetailsContainer)
+    shopItemsContainer.append(productContainer)
+
+
+
+})
