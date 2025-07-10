ecac11a7a317d7c3b03e806aff214c9fe89e6665
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-07-10 16:11:55 +0330

    init

--- javascript/session19/3/script.js
+++ javascript/session19/3/script.js
@@ -1,3 +1,6 @@
 let allProducts = [
-    {id:1, title:'Album 1', price:5, img:'./Images/Album 1.png'}
+    {id:1, title:'Album 1', price:5, img:'./Images/Album 1.png', count:1},
+    {id:2, title:'Album 2', price:15, img:'./Images/Album 2.png', count:1},
+    {id:3, title:'Album 3', price:7, img:'./Images/Album 3.png', count:1},
+    {id:4, title:'Album 4', price:9, img:'./Images/Album 4.png', count:1},
 ]
