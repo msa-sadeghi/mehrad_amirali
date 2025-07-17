7def87810b6a5828123cf8044ed6b83a83c2d292
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-07-17 16:57:58 +0330

    codes

---
+++ javascript/session20/1/script.js
@@ -0,0 +1,10 @@
+// let scrollElem = document.querySelector("#scroll")
+
+// window.addEventListener("scroll", function(){
+//     let scrollTop = this.window.scrollY;
+//     let documentheight = this.document.body.clientHeight;
+//     let windowHeight = this.window.innerHeight;
+//     scrollElem.style.width = scrollTop / (documentheight - windowHeight) * 100   + '%'
+// })
+
+window.location = "https://niklearing.ir"
