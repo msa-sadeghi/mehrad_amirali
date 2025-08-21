56b34f128f69573ff00f2aaa71d936895594fd95
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-08-21 17:01:16 +0330

    next

--- javascript/session22/1/script.js
+++ javascript/session22/1/script.js
@@ -26,3 +26,47 @@ let suggestions = [
     "What does HTML stands for?",
     "What does CSS stands for?",
 ];
+
+
+let autoComWrapper = document.querySelector('.search-input')
+let autoComBox = document.querySelector('.autocom-box')
+let inputBox = document.querySelector('input')
+
+inputBox.addEventListener('keyup', function(){
+    let searchVal = inputBox.value
+
+    if(searchVal){
+        autoComWrapper.classList.add('active')
+
+        let filteredWords = suggestions.filter(function(word){
+            return word.toLocaleLowerCase().startsWith(searchVal.toLocaleLowerCase())
+        })
+
+        suggestionWordsGenerator(filteredWords)
+
+    }else{
+        autoComWrapper.classList.remove('active')
+
+    }
+
+})
+
+function suggestionWordsGenerator(words){
+    let listItemArray = words.map(function(word){
+        return '<li>' + word +'</li>'
+    })
+    let result 
+    result = listItemArray.join('')
+
+    
+    autoComBox.innerHTML = result
+    let allListItems = autoComBox.querySelectorAll('li')
+    allListItems.forEach(function(item){
+        item.addEventListener('click', function(event){
+            inputBox.value  =  event.target.textContent
+            autoComWrapper.classList.remove('active')
+        })
+    })
+
+
+}
