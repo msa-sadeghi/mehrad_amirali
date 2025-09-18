1e04b95086a42e81a0002fec44a085cd7df8cc82
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-09-18 17:06:38 +0330

    س

---
+++ javascript/session24/app.js
@@ -0,0 +1,39 @@
+const registerForm = document.querySelector('.register-form')
+const nameInput = document.getElementById('name')
+const passwordInput = document.getElementById('password')
+const emailInput = document.getElementById('email')
+
+let db = null
+let objectStore = null
+
+window.addEventListener("load", () => {
+    let db = indexedDB.open('mytest1', 3)
+    db.addEventListener('error', (error)=>{
+        // console.warn('Error', error)
+    })
+    db.addEventListener('success', (ev)=>{
+        // console.log('Success', ev.target.result)
+    })
+
+    db.addEventListener('upgradeneeded', (event)=>{
+        db = event.target.result
+        if(!db.objectStoreNames.contains('users')){
+            objectStore = db.createObjectStore('users')
+        }
+    })
+})
+
+registerForm.addEventListener('submit', event => {
+    event.preventDefault()
+    console.log(db)
+    let newUser = {
+        userID : Math.floor(Math.random() * 99),
+        name: nameInput.value,
+        password : passwordInput.value,
+        email:emailInput.value
+    }
+
+    let tx = db.transaction('users', 'readwrite')
+    let store = tx.objectStore('users')
+    let request = store.add(newUser)
+})
