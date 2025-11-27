a1b7ad9e30bfdda6b28d2632ad8cf7be82b27039
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-11-27 16:23:48 +0330

    props

--- react/my-first-app/src/App.jsx
+++ react/my-first-app/src/App.jsx
@@ -1,15 +1,29 @@
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
+import Greeting from "./components/Greeting";
+import UserCard from "./components/UserCard";
 function App(){
   
 
   return(
-    <>
-    <Header/>
-    <MainContent/>
-    <Footer/>
-    </>
+    <div style={{display:'flex'}}>
+    {/* <Greeting name="amir"/>
+    <Greeting name="sara"/>
+    <Greeting name="mary"/>
+    <Greeting name="ali"/>
+    <Greeting name="nikan"/> */}
+
+    <UserCard
+    name="sara"
+    email="sara@gmail.com"
+    age={25}
+    city="teh"
+    />
+    <UserCard
+    name="sara"
+    email="sara@gmail.com"
+    age={25}
+    city="teh"
+    />
+    </div>
   )
 
 }
--- react/my-first-app/src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-function Header(){
-    return(
-        <header><h1>This is header</h1></header>
-    )
-}
-
-export default  Header
