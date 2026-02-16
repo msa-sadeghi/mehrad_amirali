1f697b9ce14d6f7b172847d4e9d456b67cc1dbe6
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-02-16 18:43:46 +0330

    codes

--- backend_django/blog/models.py
+++ backend_django/blog/models.py
@@ -1,13 +1,31 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="نام")
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+
 class Article(models.Model):
+    STATUS_CHOICES = (("draft", "پیش نویس"), ("published", "منتشر شده"))
     title = models.CharField(max_length=200, verbose_name="نام")
     content = models.TextField(verbose_name="محتوا")
     author = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    published = models.BooleanField(default=False)
+    # published = models.BooleanField(default=False)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="articles", default=1
+    )
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default="draft")
+    views = models.PositiveIntegerField(default=0)
+
+    def publish(self):
+        self.status = "published"
+        self.save()
 
     class Meta:
         verbose_name = "مقاله"
--- backend_django/blog/views.py
+++ backend_django/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Article
 
-# Create your views here.
+
+def index(request):
+    articles = Article.objects.all()
+    context = {"page_title": "index", "articles": articles}
+    return render(request, "blog/index.html", context)
