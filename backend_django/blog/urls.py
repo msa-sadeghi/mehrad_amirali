bd36d052b23bb7ddd9027b43d01d9f1bf5e19f62
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-02-23 18:32:13 +0330

    article_detail view added

--- backend_django/blog/urls.py
+++ backend_django/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, article_detail
 
 app_name = "blog"
 urlpatterns = [
     path("", index, name="index"),
+    path("article/<int:id>/", article_detail, name="article_detail"),
 ]
--- backend_django/blog/views.py
+++ backend_django/blog/views.py
@@ -6,3 +6,12 @@ def index(request):
     articles = Article.objects.all()
     context = {"page_title": "index", "articles": articles}
     return render(request, "blog/index.html", context)
+
+
+def article_detail(request, id):
+    article = Article.objects.get(pk=id)
+    context = {
+        'page_title' : 'article_ info',
+        'article' : article
+    }
+    return render(request, 'blog/article_detail.html', context)
