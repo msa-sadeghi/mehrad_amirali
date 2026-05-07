3cd52bcff2b8fd37263a8319dedc40933ee55129
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-05-07 16:53:48 +0330

    codes

--- react/my-first-app/src/components/SignupFrom.jsx
+++ react/my-first-app/src/components/SignupFrom.jsx
@@ -8,12 +8,34 @@ function SignupForm(){
         confirmPassword:""
     })
     const [errors, setErrors] = useState({})
-    const handleSubmit=()=>{
-
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        const validationErrors = validate()
+        if(Object.keys(validationErrors).length === 0){
+            console.log("erveything ok")
+        }else{
+            setErrors(validationErrors)
+        }
     }
+    const validate=()=>{
+        const newErrors = {}
+        if(!formData.username){
+            newErrors.username = 'username can not be empty'
+        }else if (formData.username.length < 3){
+            newErrors.username = 'username is too short'
+        }
+        if(!formData.email){
+            newErrors.email = 'email field can not be empty'
+        }else if (!/\S+@\S+\.\S+/.test(formData.email)){
+            newErrors.email = 'email is not valid'
+        }
 
+        // TODO ADD validation checks for other fields
+        return newErrors
+    }
     const handleChange = (e)=>{
-        
+        setFormData({...formData, [e.target.name]:e.target.value})
+        setErrors({...errors, [e.target.name]:''})
     }
     return(
         <form onSubmit={handleSubmit}>
@@ -25,7 +47,7 @@ function SignupForm(){
             placeholder="username..."
             onChange={handleChange}
             type="text" />   
-            {errors.username && (<p>{errors.username}</p>)}         
+            {errors.username && (<p>{errors.username}</p>)}   
             <input 
             name="email"
             value={formData.email}
